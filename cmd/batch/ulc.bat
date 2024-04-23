bada2fc625a71119414538c295214517e6459ccd
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-04-23 14:22:58 -0400

    Add ulc Windows shortcut for launching Unreal Engine game as a client

---
+++ cmd/batch/ulc.bat
@@ -0,0 +1,9 @@
+@echo off
+SET PROJECT=%1
+SET SERVER_IP=%2
+SET PORT=%3
+IF NOT DEFINED SERVER_IP SET SERVER_IP=127.0.0.1
+IF NOT DEFINED PORT SET PORT=7777
+echo Launching %PROJECT% connected to server %SERVER_IP% on port %PORT% ...
+"C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe" "%USERPROFILE%\Documents\Unreal Projects\%PROJECT%\%PROJECT%.uproject" %SERVER_IP%:%PORT% -game -log
+
