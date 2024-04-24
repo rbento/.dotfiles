32c80c64e06579192d9b44e3ccb78f203d1d7955
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-04-23 22:52:52 -0400

    Update launch messages

--- cmd/batch/uls.bat
+++ cmd/batch/uls.bat
@@ -3,5 +3,5 @@ SET PROJECT=%1
 SET MAP=%2
 IF NOT DEFINED MAP SET MAP=Main
 SET MAP=/Game/%1/%MAP%?listen
-echo Launching '%PROJECT%' on map '%MAP%' ...
+echo Launching '%PROJECT%' server on map '%MAP%' ...
 "C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe" "%USERPROFILE%\Documents\Unreal Projects\%PROJECT%\%PROJECT%.uproject" %MAP% -server -log
