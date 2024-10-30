79f1b7b1ab68f936950cf0db7cb69647bc69c81a
Rodrigo Bento <rodrigo.bento@gmail.com>
2024-10-30 17:32:06 -0400

    Delete init-remove-win-components.bat

--- init-remove-win-components.bat
+++
@@ -1,4 +0,0 @@
-Get-AppxPackage *XboxGamingOverlay* | Remove-AppxPackage
-Get-AppxPackage *DevHome* | Remove-AppxPackage                                                    
-Get-AppxPackage *CrossDevice* | Remove-AppxPackage
-Get-AppxPackage *Phone* | Remove-AppxPackage
