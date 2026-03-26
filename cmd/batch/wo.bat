0ab43560a2ade2d5d8547d988c98686db75ba9cb
Rodrigo Bento <rodrigo.bento@gmail.com>
2026-03-26 10:32:27 -0400

    Enable wo command to switch directories across drives

--- cmd/batch/wo.bat
+++ cmd/batch/wo.bat
@@ -1,2 +1,8 @@
-@echo off
-cd %USERPROFILE%\Workspace
+@ECHO OFF
+
+IF "%USER_PROJECTS_HOME%" == "" (
+    ECHO ABORTED: Environment Variable 'USER_PROJECTS_HOME' is not defined
+    EXIT /B 1
+)
+
+cd /D %USER_PROJECTS_HOME%
